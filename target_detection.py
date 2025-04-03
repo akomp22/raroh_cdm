578699539dd1cfa03a88eee92757a176a1dd81b6
Kompanets <a.kompanets@tue.nl>
2025-04-03 13:34:12 +0200

    laptop

--- target_detection.py
+++ target_detection.py
@@ -5,7 +5,7 @@ from camera import Camera
 def find_red_spot_center(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_red1 = np.array([0, 200, 110])
+    lower_red1 = np.array([0, 150, 110])
     upper_red1 = np.array([5, 255, 255])
     lower_red2 = np.array([175, 150, 150])
     upper_red2 = np.array([180, 255, 255])
