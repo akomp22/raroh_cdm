983b1a98fa7ee26e09b58215bfdd22d63f560c3c
Kompanets <a.kompanets@tue.nl>
2025-04-04 17:19:28 +0200

    laptop

--- control_simple.py
+++ control_simple.py
@@ -81,82 +81,88 @@ if __name__ == '__main__':
     angle_ch1_rad_prev = 0.0
     angle_ch2_rad_prev = 0.0
     time.sleep(4)
-    while True:
-        ret, frame = cam.get_frame()
-        coord, mask_cleaned = find_red_spot_center(frame, cx, cy)
-        t1 = time.time()
+    try:
+        while True:
+            ret, frame = cam.get_frame()
+            coord, mask_cleaned = find_red_spot_center(frame, cx, cy)
+            t1 = time.time()
 
-        # droput protection
-        last_seen_time = time.time()
-        if coord:
-            last_coord = coord
+            # droput protection
             last_seen_time = time.time()
-        elif time.time() - last_seen_time < 0.5:
-            coord = last_coord
-        else:
-            continue
-        
-        # Filter coordinates
-        coord_filtered = COORD_ALPHA * np.array(coord) + (1 - COORD_ALPHA) * np.array(prev_coord)
-        prev_coord = coord_filtered
-
-        # calculate angle to target 
-        angle_ch1_rad = np.arctan(coord_filtered[0] / fx)  # Horizontal angle
-        angle_ch2_rad = np.arctan(coord_filtered[1] / fy)  # Vertical angle
-
-        # visual guidance system
-        aoa_ssa = mavlink_wrapper.messages["AOA_SSA"]
-        aoa = aoa_ssa.AOA
-        ssa = aoa_ssa.SSA
-        cmd_ch1 = pid_ch1.get_command(setpoint = ssa, current_value = angle_ch1_rad, current_time = time.time())
-        cmd_ch2 = pid_ch2.get_command(setpoint = aoa, current_value = angle_ch2_rad, current_time = time.time())
-
-        # PN system
-        dt = time.time() - t1
-        d_error_ch1 = (angle_ch1_rad - angle_ch1_rad_prev)  / dt if dt > 0 else 0.0
-        d_error_ch2 = (angle_ch2_rad - angle_ch2_rad_prev)  / dt if dt > 0 else 0.0
-
-        pn_term_ch1 = -NAV_GAIN * d_error_ch1
-        pn_term_ch2 = -NAV_GAIN * d_error_ch2
-        cmd_ch1 += pn_term_ch1
-        cmd_ch2 += pn_term_ch2
-        angle_ch1_rad_prev = angle_ch1_rad
-        angle_ch2_rad_prev = angle_ch2_rad
-
-        # execute command
-        if REVERSED_CH1:
-            cmd_ch1 = -cmd_ch1
-        if REVERSED_CH2:
-            cmd_ch2 = -cmd_ch2
-        cmd_ch1 = int(rc1_trim+cmd_ch1)
-        cmd_ch2 = int(rc2_trim+cmd_ch2)
-        cmd_ch1 = max(min(cmd_ch1, MAC_CH1), MIN_CH1)
-        cmd_ch2 = max(min(cmd_ch2, MAC_CH2), MIN_CH1)
-        print(f"angle_x_rad {angle_ch1_rad:.3f} angle_y_rad {angle_ch2_rad:.3f}; cmd x {cmd_ch1}; cmd y {cmd_ch2}")
-        mavlink_wrapper.set_rc_channel_pwm([1,2], [cmd_ch1, cmd_ch2])
-
-        if SAVE_DATA:
-            logger.add_scalar("cmd_ch1", cmd_ch1, time.time())
-            logger.add_scalar("cmd_ch2", cmd_ch2, time.time())
-            logger.add_scalar("angle_ch1_rad", angle_ch1_rad, time.time())
-            logger.add_scalar("angle_ch2_rad", angle_ch2_rad, time.time())
-            logger.add_scalar("coord_x", coord[0], time.time())
-            logger.add_scalar("coord_y", coord[1], time.time())
-            logger.add_scalar("coord_x_filtered", coord_filtered[0], time.time())
-            logger.add_scalar("coord_y_filtered", coord_filtered[1], time.time())
-            logger.add_scalar("aoa", aoa, time.time())
-            logger.add_scalar("ssa", ssa, time.time())
-            logger.add_scalar("pn_term_ch1", pn_term_ch1, time.time())
-            logger.add_scalar("pn_term_ch2", pn_term_ch2, time.time())
-            logger.add_scalar("d_error_ch1", d_error_ch1, time.time())
-            logger.add_scalar("d_error_ch2", d_error_ch2, time.time())
-            logger.add_scalar("dt", dt, time.time())
-
-            # add coord to the frame
             if coord:
-                disp_coord = (int(coord[0] + cx), int(coord[1] + cy))
-                cv2.circle(frame, disp_coord, 5, (0, 255, 0), -1)
-            logger.add_frame_to_video("frame", frame, fps=30)
+                last_coord = coord
+                last_seen_time = time.time()
+            elif time.time() - last_seen_time < 0.5:
+                coord = last_coord
+            else:
+                continue
+            
+            # Filter coordinates
+            coord_filtered = COORD_ALPHA * np.array(coord) + (1 - COORD_ALPHA) * np.array(prev_coord)
+            prev_coord = coord_filtered
+
+            # calculate angle to target 
+            angle_ch1_rad = np.arctan(coord_filtered[0] / fx)  # Horizontal angle
+            angle_ch2_rad = np.arctan(coord_filtered[1] / fy)  # Vertical angle
+
+            # visual guidance system
+            aoa_ssa = mavlink_wrapper.messages["AOA_SSA"]
+            aoa = aoa_ssa.AOA
+            ssa = aoa_ssa.SSA
+            cmd_ch1 = pid_ch1.get_command(setpoint = ssa, current_value = angle_ch1_rad, current_time = time.time())
+            cmd_ch2 = pid_ch2.get_command(setpoint = aoa, current_value = angle_ch2_rad, current_time = time.time())
+
+            # PN system
+            dt = time.time() - t1
+            d_error_ch1 = (angle_ch1_rad - angle_ch1_rad_prev)  / dt if dt > 0 else 0.0
+            d_error_ch2 = (angle_ch2_rad - angle_ch2_rad_prev)  / dt if dt > 0 else 0.0
+
+            pn_term_ch1 = -NAV_GAIN * d_error_ch1
+            pn_term_ch2 = -NAV_GAIN * d_error_ch2
+            cmd_ch1 += pn_term_ch1
+            cmd_ch2 += pn_term_ch2
+            angle_ch1_rad_prev = angle_ch1_rad
+            angle_ch2_rad_prev = angle_ch2_rad
+
+            # execute command
+            if REVERSED_CH1:
+                cmd_ch1 = -cmd_ch1
+            if REVERSED_CH2:
+                cmd_ch2 = -cmd_ch2
+            cmd_ch1 = int(rc1_trim+cmd_ch1)
+            cmd_ch2 = int(rc2_trim+cmd_ch2)
+            cmd_ch1 = max(min(cmd_ch1, MAC_CH1), MIN_CH1)
+            cmd_ch2 = max(min(cmd_ch2, MAC_CH2), MIN_CH1)
+            print(f"angle_x_rad {angle_ch1_rad:.3f} angle_y_rad {angle_ch2_rad:.3f}; cmd x {cmd_ch1}; cmd y {cmd_ch2}")
+            mavlink_wrapper.set_rc_channel_pwm([1,2], [cmd_ch1, cmd_ch2])
+
+            if SAVE_DATA:
+                logger.add_scalar("cmd_ch1", cmd_ch1, time.time())
+                logger.add_scalar("cmd_ch2", cmd_ch2, time.time())
+                logger.add_scalar("angle_ch1_rad", angle_ch1_rad, time.time())
+                logger.add_scalar("angle_ch2_rad", angle_ch2_rad, time.time())
+                logger.add_scalar("coord_x", coord[0], time.time())
+                logger.add_scalar("coord_y", coord[1], time.time())
+                logger.add_scalar("coord_x_filtered", coord_filtered[0], time.time())
+                logger.add_scalar("coord_y_filtered", coord_filtered[1], time.time())
+                logger.add_scalar("aoa", aoa, time.time())
+                logger.add_scalar("ssa", ssa, time.time())
+                logger.add_scalar("pn_term_ch1", pn_term_ch1, time.time())
+                logger.add_scalar("pn_term_ch2", pn_term_ch2, time.time())
+                logger.add_scalar("d_error_ch1", d_error_ch1, time.time())
+                logger.add_scalar("d_error_ch2", d_error_ch2, time.time())
+                logger.add_scalar("dt", dt, time.time())
+
+                # add coord to the frame
+                if coord:
+                    disp_coord = (int(coord[0] + cx), int(coord[1] + cy))
+                    cv2.circle(frame, disp_coord, 5, (0, 255, 0), -1)
+                logger.add_frame_to_video("frame", frame, fps=30)
+    except :
+        logger.close()
+        mavlink_wrapper.close()
+        cam.release()
+        print("Exiting...")
         
 
 
